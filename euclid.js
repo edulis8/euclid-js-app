5051a333643a4471361b1fe8844c33df43f1d7d9
edulis8 <eric_broberg@hotmail.com>
2015-09-10 21:12:58 -0700

    fix

--- euclid.js
+++ euclid.js
@@ -1,33 +1,3 @@
-$(document).ready(function(){
-
-	$("#jbutton").click(function(){
-		$("#javascript").html("<div class=jumbotron>var merge = function(array1, array2, callback){ 
-
-    var mergedArr = [];
-  
-  	for(var i = 0; i<array1.length; i++){
-  		mergedArr.push(callback(array1[i], array2[i]));
-  	
-  }
-
-  return mergedArr;
-
-};
-
-//a2+b2 = c2
-
-var euclid = function(coords1, coords2){
-	var squaredDeltas = merge(coords1, coords2, function(a,b){
-		return Math.abs((a-b)*(a-b));
-	});
-
-	var answer = Math.sqrt(squaredDeltas[0] + squaredDeltas[1]);
-
-	return answer;
-};</div>")
-
-
-	})// end click
 
 
 
@@ -59,5 +29,3 @@ var euclid = function(coords1, coords2){
 var x = 12345;
 
 
-
-}) // end of doc ready
