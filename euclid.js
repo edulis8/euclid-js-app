ca7d01a1a8e2418e698ebc4e2f0891a0157003c8
edulis8 <eric_broberg@hotmail.com>
2015-11-16 21:49:37 -0800

    show code feature

--- euclid.js
+++ euclid.js
@@ -1,4 +1,7 @@
 
+$(document).ready(function() {
+  
+ /* define merge function, which takes two arrays of numbers of identical length and a callback function */
 
 
 var merge = function(array1, array2, callback){ 
@@ -15,6 +18,7 @@ var merge = function(array1, array2, callback){
 };
 
 //a2+b2 = c2
+ /* now, use your merge function to complete the euclid function. Euclid should take two arrays, each of which has an equal number of numerical elements, and return the euclidean distance between them. (This distance is found using the pythagorean theorem a*a + b*b = c*c) */
 
 var euclid = function(coords1, coords2){
 	var squaredDeltas = merge(coords1, coords2, function(a,b){
@@ -26,6 +30,16 @@ var euclid = function(coords1, coords2){
 	return answer;
 };
 
-var x = 12345;
+
+//JQUERY
+
+$(".show").click(function(event) {
+  $(".demo").fadeIn();
+});
+
+
+
+
+});
 
 
